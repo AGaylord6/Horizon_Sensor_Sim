764345ceecf687115a3f8aa2011df8dc2cb8edba
Andrew Gaylord <113562174+AGaylord6@users.noreply.github.com>
2025-03-23 14:51:53 -0400

    fixed imoprt erorrr

--- Simulator/magnetorquer.py
+++ Simulator/magnetorquer.py
@@ -5,6 +5,7 @@ Author: Daniel, Brian, Sophie
 ??
 '''
 # TODO: add docstring
+from Horizon_Sensor_Sim.params import *
 
 class Magnetorquer():
     '''Class to hold a single magnetorquer'''
@@ -23,6 +24,13 @@ class Magnetorquer():
         self.k = k
         # accounts for the magnetic permeability and the geometry of the core. 1 for air-core magnetorquers
         self.epsilon = epsilon
+
+        if (self.epsilon == 1) :
+            self.resistance = AIR_RESISTANCE_MAG
+            self.inductance = AIR_INDUCTANCE_MAG
+        else:
+            self.resistance = FERRO_RESISTANCE_MAG
+            self.inductance = FERRO_INDUCTANCE_MAG
 
     def __repr__(self): # representation, returns string with details on the magnetorquer when "mag_object_name" is called
         if (self.epsilon == 1) :
